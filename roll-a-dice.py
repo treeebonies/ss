2ed3cdfaa6ce07958c300a3a9ff22ca5a448aff9
treeebonies <82763578+treeebonies@users.noreply.github.com>
2022-11-15 20:57:24 -0500

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,20 @@
+import random
+    response = y
+    if True:
+        print("random dice")
+        response == y:
+
+no = random.randint(1,6)
+conditiput.on = 0
+    if random : 1
+    print("1")
+    if random : 2
+    print("2")
+    if random : 3
+    print("3")
+    if random : 4
+    print("4")
+    if random : 5
+    print("5")
+    if random : 6
+    print("6")
